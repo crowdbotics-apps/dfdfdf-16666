b5704310117092185520ea6b0487f3f2d6f3d0eb
Crowdbotics <team@crowdbotics.com>
2020-05-06 12:45:14 +0000

    Initial commit.

---
+++ dfdfdf_16666/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for dfdfdf_16666 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dfdfdf_16666.settings')
+
+application = get_wsgi_application()
